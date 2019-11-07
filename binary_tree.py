1a4551ef980b744a3b44a66d879e2569dcc2fbdd
Rachel Bowyer <rabo8409@colorado.edu>
2019-11-07 17:09:57 +0000

    added insert_key_value_pairs.py file and fixed binary tree

--- binary_tree.py
+++ binary_tree.py
@@ -1,181 +1,266 @@
 class Node:
     def __init__(self, key, value=None, left=None, right=None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
 
-def insert(root, key, value=None):
-    return root
-
+def add(root, key, value=None):
+    if root == None:
+        root = Node(key, value=value)
+        return root
+    else: 
+        if key < root.key:
+            if root.left == None:
+                root.left = Node(key, value=value)
+            else:
+                add(root.left, key, value=value)
+        else:
+            if root.right == None:
+                root.right = Node(key, value=value)
+            else:
+                add(root.right, key, value=value)
+        return root
+    
 def search(root, key):
-    return None
+    if root is None: 
+        #print('key not found')
+        return root
+    
+    elif root.key == key:
+        #print('value is '+str(root.value))
+        return root.value
+
+    # Key is greater than root's key 
+    elif root.key < key: 
+        #print('searching right')
+        return search(root.right,key) 
+
+    # Key is smaller than root's key 
+    else:
+        #print('searching left')
+        return search(root.left,key) 
+        #return None
+
+# if __name__ == '__main__':
+
+#     root = None
+
+#     root = add(root, 10)
+#     root = add(root, 9)
+#     root = add(root, 15)
+#     root = add(root, 11)
+
+#     print(root.key, root.value)
+#     print(root.left.key, root.left.value)
+#     print(root.right.left.key)
 
 
-class Node:
-	def __init__(self,value=None):
-		self.value=value
-		self.left_child=None
-		self.right_child=None
-		self.parent=None # pointer to parent node in tree
+# class Node: 
+#     def __init__(self,key): 
+#         self.left = None
+#         self.right = None
+#         self.val = key 
 
-class binary_search_tree:
-	def __init__(self):
-		self.root=None
+# # A utility function to insert a new node with the given key 
+# def insert(root,node): 
+#     if root is None: 
+#         root = node 
+#     else:
+#         print(root)
+#         print(node)
+#         if root.val < node.val: 
+#             if root.right is None: 
+#                 root.right = node 
+#             else: 
+#                 insert(root.right, node) 
+#         else: 
+#             if root.left is None: 
+#                 root.left = node 
+#             else: 
+#                 insert(root.left, node)
 
-	def insert(self,value):
-		if self.root==None:
-			self.root=node(value)
-		else:
-			self._insert(value,self.root)
+# def search(root, key):
+#     if root is None or root.value == key: 
+#         return root 
 
-	def _insert(self,value,cur_node):
-		if value<cur_node.value:
-			if cur_node.left_child==None:
-				cur_node.left_child=node(value)
-				cur_node.left_child.parent=cur_node # set parent
-			else:
-				self._insert(value,cur_node.left_child)
-		elif value>cur_node.value:
-			if cur_node.right_child==None:
-				cur_node.right_child=node(value)
-				cur_node.right_child.parent=cur_node # set parent
-			else:
-				self._insert(value,cur_node.right_child)
-		else:
-			print("Value already in tree!")
+#     # Key is greater than root's key 
+#     if root.value < key: 
+#         return search(root.right,key) 
 
-	def print_tree(self):
-		if self.root!=None:
-			self._print_tree(self.root)
+#     # Key is smaller than root's key 
+#     return search(root.left,key) 
+#         #return None
 
-	def _print_tree(self,cur_node):
-		if cur_node!=None:
-			self._print_tree(cur_node.left_child)
-			print (str(cur_node.value))
-			self._print_tree(cur_node.right_child)
 
-	def height(self):
-		if self.root!=None:
-			return self._height(self.root,0)
-		else:
-			return 0
+# # class Node:
+# # 	def __init__(self,value=None):
+# # 		self.value=value
+# # 		self.left_child=None
+# # 		self.right_child=None
+# # 		self.parent=None # pointer to parent node in tree
 
-	def _height(self,cur_node,cur_height):
-		if cur_node==None: return cur_height
-		left_height=self._height(cur_node.left_child,cur_height+1)
-		right_height=self._height(cur_node.right_child,cur_height+1)
-		return max(left_height,right_height)
+# # class binary_search_tree:
+# # 	def __init__(self):
+# # 		self.root=None
 
-	def find(self,value):
-		if self.root!=None:
-			return self._find(value,self.root)
-		else:
-			return None
+# # 	def insert(self,value):
+# # 		if self.root==None:
+# # 			self.root=node(value)
+# # 		else:
+# # 			self._insert(value,self.root)
 
-	def _find(self,value,cur_node):
-		if value==cur_node.value:
-			return cur_node
-		elif value<cur_node.value and cur_node.left_child!=None:
-			return self._find(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
-			return self._find(value,cur_node.right_child)
+# # 	def _insert(self,value,cur_node):
+# # 		if value<cur_node.value:
+# # 			if cur_node.left_child==None:
+# # 				cur_node.left_child=node(value)
+# # 				cur_node.left_child.parent=cur_node # set parent
+# # 			else:
+# # 				self._insert(value,cur_node.left_child)
+# # 		elif value>cur_node.value:
+# # 			if cur_node.right_child==None:
+# # 				cur_node.right_child=node(value)
+# # 				cur_node.right_child.parent=cur_node # set parent
+# # 			else:
+# # 				self._insert(value,cur_node.right_child)
+# # 		else:
+# # 			print("Value already in tree!")
 
-	def delete_value(self,value):
-		return self.delete_node(self.find(value))
+# # 	def print_tree(self):
+# # 		if self.root!=None:
+# # 			self._print_tree(self.root)
 
-	def delete_node(self,node):
+# # 	def _print_tree(self,cur_node):
+# # 		if cur_node!=None:
+# # 			self._print_tree(cur_node.left_child)
+# # 			print (str(cur_node.value))
+# # 			self._print_tree(cur_node.right_child)
 
-		## -----
-		# Improvements since prior lesson
+# # 	def height(self):
+# # 		if self.root!=None:
+# # 			return self._height(self.root,0)
+# # 		else:
+# # 			return 0
 
-		# Protect against deleting a node not found in the tree
-		if node==None or self.find(node.value)==None:
-			print("Node to be deleted not found in the tree!")
-			return None 
-		## -----
+# # 	def _height(self,cur_node,cur_height):
+# # 		if cur_node==None: return cur_height
+# # 		left_height=self._height(cur_node.left_child,cur_height+1)
+# # 		right_height=self._height(cur_node.right_child,cur_height+1)
+# # 		return max(left_height,right_height)
 
-		# returns the node with min value in tree rooted at input node
-		def min_value_node(n):
-			current=n
-			while current.left_child!=None:
-				current=current.left_child
-			return current
+# # 	def find(self,value):
+# # 		if self.root!=None:
+# # 			return self._find(value,self.root)
+# # 		else:
+# # 			return None
 
-		# returns the number of children for the specified node
-		def num_children(n):
-			num_children=0
-			if n.left_child!=None: num_children+=1
-			if n.right_child!=None: num_children+=1
-			return num_children
+# # 	def _find(self,value,cur_node):
+# # 		if value==cur_node.value:
+# # 			return cur_node
+# # 		elif value<cur_node.value and cur_node.left_child!=None:
+# # 			return self._find(value,cur_node.left_child)
+# # 		elif value>cur_node.value and cur_node.right_child!=None:
+# # 			return self._find(value,cur_node.right_child)
 
-		# get the parent of the node to be deleted
-		node_parent=node.parent
+# # 	def delete_value(self,value):
+# # 		return self.delete_node(self.find(value))
 
-		# get the number of children of the node to be deleted
-		node_children=num_children(node)
+# # 	def delete_node(self,node):
 
-		# break operation into different cases based on the
-		# structure of the tree & node to be deleted
+# # 		## -----
+# # 		# Improvements since prior lesson
 
-		# CASE 1 (node has no children)
-		if node_children==0:
+# # 		# Protect against deleting a node not found in the tree
+# # 		if node==None or self.find(node.value)==None:
+# # 			print("Node to be deleted not found in the tree!")
+# # 			return None 
+# # 		## -----
 
-			# Added this if statement post-video, previously if you 
-			# deleted the root node it would delete entire tree.
-			if node_parent!=None:
-				# remove reference to the node from the parent
-				if node_parent.left_child==node:
-					node_parent.left_child=None
-				else:
-					node_parent.right_child=None
-			else:
-				self.root=None
+# # 		# returns the node with min value in tree rooted at input node
+# # 		def min_value_node(n):
+# # 			current=n
+# # 			while current.left_child!=None:
+# # 				current=current.left_child
+# # 			return current
 
-		# CASE 2 (node has a single child)
-		if node_children==1:
+# # 		# returns the number of children for the specified node
+# # 		def num_children(n):
+# # 			num_children=0
+# # 			if n.left_child!=None: num_children+=1
+# # 			if n.right_child!=None: num_children+=1
+# # 			return num_children
 
-			# get the single child node
-			if node.left_child!=None:
-				child=node.left_child
-			else:
-				child=node.right_child
+# # 		# get the parent of the node to be deleted
+# # 		node_parent=node.parent
 
-			# Added this if statement post-video, previously if you 
-			# deleted the root node it would delete entire tree.
-			if node_parent!=None:
-				# replace the node to be deleted with its child
-				if node_parent.left_child==node:
-					node_parent.left_child=child
-				else:
-					node_parent.right_child=child
-			else:
-				self.root=child
+# # 		# get the number of children of the node to be deleted
+# # 		node_children=num_children(node)
 
-			# correct the parent pointer in node
-			child.parent=node_parent
+# # 		# break operation into different cases based on the
+# # 		# structure of the tree & node to be deleted
 
-		# CASE 3 (node has two children)
-		if node_children==2:
+# # 		# CASE 1 (node has no children)
+# # 		if node_children==0:
 
-			# get the inorder successor of the deleted node
-			successor=min_value_node(node.right_child)
+# # 			# Added this if statement post-video, previously if you 
+# # 			# deleted the root node it would delete entire tree.
+# # 			if node_parent!=None:
+# # 				# remove reference to the node from the parent
+# # 				if node_parent.left_child==node:
+# # 					node_parent.left_child=None
+# # 				else:
+# # 					node_parent.right_child=None
+# # 			else:
+# # 				self.root=None
 
-			# copy the inorder successor's value to the node formerly
-			# holding the value we wished to delete
-			node.value=successor.value
+# # 		# CASE 2 (node has a single child)
+# # 		if node_children==1:
 
-			# delete the inorder successor now that it's value was
-			# copied into the other node
-			self.delete_node(successor)
+# # 			# get the single child node
+# # 			if node.left_child!=None:
+# # 				child=node.left_child
+# # 			else:
+# # 				child=node.right_child
 
-	def search(self,value):
-		if self.root!=None:
-			return self._search(value,self.root)
-		else:
-			return False
+# # 			# Added this if statement post-video, previously if you 
+# # 			# deleted the root node it would delete entire tree.
+# # 			if node_parent!=None:
+# # 				# replace the node to be deleted with its child
+# # 				if node_parent.left_child==node:
+# # 					node_parent.left_child=child
+# # 				else:
+# # 					node_parent.right_child=child
+# # 			else:
+# # 				self.root=child
 
-	def _search(self,value,cur_node):
-		if value==cur_node.value:
-			return True
-		elif value<cur_node.value and cur_node.left_child!=None:
-			return self._search(value,cur_node.left_child)
-		elif value>cur_node.value and cur_node.right_child!=None:
-			return self._search(value,cur_node.right_child)
-		return False
+# # 			# correct the parent pointer in node
+# # 			child.parent=node_parent
+
+# # 		# CASE 3 (node has two children)
+# # 		if node_children==2:
+
+# # 			# get the inorder successor of the deleted node
+# # 			successor=min_value_node(node.right_child)
+
+# # 			# copy the inorder successor's value to the node formerly
+# # 			# holding the value we wished to delete
+# # 			node.value=successor.value
+
+# # 			# delete the inorder successor now that it's value was
+# # 			# copied into the other node
+# # 			self.delete_node(successor)
+
+# # 	def search(self,value):
+# # 		if self.root!=None:
+# # 			return self._search(value,self.root)
+# # 		else:
+# # 			return False
+
+# # 	def _search(self,value,cur_node):
+# # 		if value==cur_node.value:
+# # 			return True
+# # 		elif value<cur_node.value and cur_node.left_child!=None:
+# # 			return self._search(value,cur_node.left_child)
+# # 		elif value>cur_node.value and cur_node.right_child!=None:
+# # 			return self._search(value,cur_node.right_child)
+# # 		return False
